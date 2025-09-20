45a1caba6ecf7500641dc6f9c1f6bc666938762e
belgis <belgisanggita282@gmail.com>
2025-09-20 10:18:34 +0700

    new commit

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running document ingestion..."
-python ingest.py
+python app/ingest.py
 
 echo "Starting API server..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
